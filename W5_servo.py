588e537032975364f04809b3f968a8a42a714d6b
eleanor <eleanor128@gmail.com>
2025-09-30 14:21:56 +0800

    更新PWM servo

--- W5_servo.py
+++ W5_servo.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-servoPIN = 12
+servoPIN = 13
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p = GPIO.PWM(servoPIN, 50) # GPIO 13 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
   while True:
